b01c70ee327e112996937990e180b93d87d69bb6
Johan Nordström <johan@digitalvillage.se>
2026-04-27 10:57:37 +0200

    Another stateful variable 💪🏻.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -3,6 +3,7 @@ import "./assets/scss/App.scss";
 
 function App() {
 	const [counter, setCounter] = useState(0);
+	const [msg, setMsg] = useState("Hi mom!");
 
 	const handleBtnClick = () => {
 		console.log("Counter before update:", counter);
@@ -15,6 +16,12 @@ function App() {
 	return (
 		<div className="container">
 			<h1>01-react-basics</h1>
+
+			<p>{msg}</p>
+
+			<button onClick={ () => setMsg("Hi dad!") } className="btn btn-warning">Hi dad?</button>
+
+			<hr />
 
 			<p>Counter: {counter}</p>
 
