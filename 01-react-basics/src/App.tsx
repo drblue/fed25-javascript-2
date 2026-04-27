6cb27aa66d018f6c7d9ae44c014b2471fa33fc5f
Johan Nordström <johan@digitalvillage.se>
2026-04-27 11:15:10 +0200

    Render a list 🤘🏻.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import "./assets/scss/App.scss";
 
+interface Post {
+	id: number;
+	title: string;
+	likes: number;
+}
+
 function App() {
 	const [counter, setCounter] = useState(0);
 	const [msg, setMsg] = useState("Hi mom!");
+	const [posts, setPosts] = useState<Post[]>([
+		{ id: 1, title: "React Rocks 🎸", likes: 1337 },
+		{ id: 2, title: "JSX Rocks Even Moar 🤘🏻", likes: 42 },
+		{ id: 3, title: "Got State?", likes: 3 },
+	]);
 
 	const handleBtnClick = () => {
 		console.log("Counter before update:", counter);
@@ -26,6 +37,18 @@ function App() {
 			<p>Counter: {counter}</p>
 
 			<button onClick={handleBtnClick} className="btn btn-primary">Click me!</button>
+
+			<hr />
+
+			<h2>Posts</h2>
+
+			<ul>
+				{posts.map(post =>
+					<li key={post.id}>
+						{post.title} ({post.likes} likes)
+					</li>
+				)}
+			</ul>
 		</div>
 	);
 }
