71ed7027dc92aeb597951b7191c1aa866d2b0f2a
Johan Nordström <johan@digitalvillage.se>
2026-04-29 10:22:50 +0200

    Moved salary logic to its own component. State is being reset to `initialState` on every component mount.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Counter from "./components/Counter";
+import Salary from "./components/Salary";
 import "./assets/scss/App.scss";
 
 interface Post {
@@ -15,16 +16,7 @@ function App() {
 		{ id: 2, title: "JSX Rocks Even Moar 🤘🏻", likes: 42 },
 		{ id: 3, title: "Got State?", likes: 3 },
 	]);
-	const [salary, setSalary] = useState(10);
 	const [showSalary, setShowSalary] = useState(false);
-
-	const handleChangeSalary = (amount: number) => {
-		if (salary + amount < 5) {
-			setSalary(5);
-			return;
-		}
-		setSalary(salary + amount);
-	}
 
 	const handleDeletePost = (postToDelete: Post) => {
 		setPosts(posts.filter(post => post !== postToDelete));
@@ -63,51 +55,7 @@ function App() {
 				{showSalary ? "Hide salary" : "Show salary"}
 			</button>
 
-			{showSalary && (
-				<>
-					<h2>Salary</h2>
-
-					<p>Salary per hour: {salary} &euro;</p>
-
-					{salary < 10 && (
-						<div className="alert alert-warning">
-							You might want to change job?
-						</div>
-					)}
-
-					<div className="buttons">
-						<div className="mb-1">
-							<button
-								className="btn btn-primary btn-lg"
-								onClick={() => handleChangeSalary(1)}
-							>
-								Raise 1 &euro; 🤑
-							</button>
-							<button
-								className="btn btn-warning btn-lg"
-								onClick={() => handleChangeSalary(-1)}
-							>
-								Decrease 1 &euro; 😢
-							</button>
-						</div>
-
-						<div className="mb-1">
-							<button
-								className="btn btn-primary btn-lg"
-								onClick={() => handleChangeSalary(5)}
-							>
-								Raise 5 &euro; 🤑🤑🤑
-							</button>
-							<button
-								className="btn btn-warning btn-lg"
-								onClick={() => handleChangeSalary(-5)}
-							>
-								Decrease 5 &euro; 😢😢😢
-							</button>
-						</div>
-					</div>
-				</>
-			)}
+			{showSalary && <Salary />}
 
 			<hr />
 
