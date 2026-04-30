5f93023b8a7b1b0b63891ab9388c30b83eba5405
Johan Nordström <johan@digitalvillage.se>
2026-04-30 09:33:28 +0200

    Added state for todolist and rendered list.

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import type { Todo } from "./types/Todo";
 import "./assets/scss/App.scss";
 
 function App() {
+	const [todos, setTodos] = useState<Todo[]>([
+		{ id: 1, title: "Make coffee", completed: true },
+		{ id: 2, title: "Drink coffee", completed: false },
+		{ id: 3, title: "Drink MOAR coffee", completed: false },
+		{ id: 4, title: "Drink ALL ZE coffee", completed: false },
+	]);
+
 	return (
 		<div className="container py-3">
 			<h1>React Simple Todos</h1>
+
+			<ul className="todolist list-group">
+				{todos.map(todo => (
+					<li
+						key={todo.id}
+						className={todo.completed ? "completed list-group-item" : "list-group-item"}
+					>
+						<span className="todo-title">{todo.title}</span>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
