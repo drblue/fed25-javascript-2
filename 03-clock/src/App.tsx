fd332b9cf32fd036a62034698d96d0af0e9dbe37
Johan Nordström <johan@digitalvillage.se>
2026-04-30 11:37:40 +0200

    Working clock 🕰️.

--- 03-clock/src/App.tsx
+++ 03-clock/src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import "./assets/scss/App.scss";
 
 function App() {
+	const [time, setTime] = useState("00:00:00");
+
+	useEffect(() => {
+		console.log("🔫 Starting clock...");
+		setInterval(() => {
+			const now = new Date().toLocaleTimeString();
+			setTime(now);
+			console.log("🕰️ Tick...", now);
+		}, 1000);
+	}, []);
+
+	console.log("App is rendering...");
+
 	return (
 		<div className="container">
 			<div className="display-1 font-monospace text-center">
-				00:13:37
+				{time}
 			</div>
 		</div>
 	);
