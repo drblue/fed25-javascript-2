d5aec9679f0bdaa2ad8cf2787b172248abdefe84
Johan Nordström <johan@digitalvillage.se>
2026-05-06 09:33:27 +0200

    Exercise: Added error- and loading state.

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -7,6 +7,8 @@ import type { Todo } from "./types/Todo";
 import "./assets/scss/App.scss";
 
 function App() {
+	const [error, setError] = useState<string | false>(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [todos, setTodos] = useState<Todo[]>([]);
 
 	const handleAddTodo = (title: string) => {
@@ -27,11 +29,20 @@ function App() {
 	useEffect(() => {
 		const getTodos = async () => {
 			// reset state
+			setError(false);
+			setIsLoading(true);
 			setTodos([]);
 
 			// make request to api
-			const data = await TodoAPI.getTodos();
-			setTodos(data);
+			try {
+				const data = await TodoAPI.getTodos();
+				setTodos(data);
+			} catch (err) {
+				console.error("Error thrown when fetching todos:", err);
+				setError(err instanceof Error ? err.message : "It's not me, it's you");
+			} finally {
+				setIsLoading(false);
+			}
 		}
 		getTodos();
 	}, [])
@@ -41,35 +52,48 @@ function App() {
 			<title>{`${uncompletedTodos.length} of ${todos.length} todos left`}</title>
 			<h1>React Simple Todos</h1>
 
-			<AddNewTodoForm onAdd={handleAddTodo} />
-
-			{todos.length > 0 ? (
-				<>
-					<h2 className="h5">💪🏻 Stuff I got to do</h2>
-					<TodoList
-						onDelete={handleDelete}
-						onToggle={handleToggle}
-						todos={uncompletedTodos}
-					/>
-
-					<h2 className="h5">🥺 Stuff I've done</h2>
-					<TodoList
-						onDelete={handleDelete}
-						onToggle={handleToggle}
-						todos={completedTodos}
-					/>
-
-					<TodoCounter
-						total={todos.length}
-						uncompleted={uncompletedTodos.length}
-					/>
-				</>
-			) : (
-				<div className="alert alert-warning">
-					You ain't got no todos to do? 🤔
+			{error && (
+				<div className="alert alert-danger">
+					{error}
 				</div>
 			)}
 
+			{isLoading && (
+				<div className="spinner-border" role="status">
+					<span className="visually-hidden">Loading...</span>
+				</div>
+			)}
+
+			{!error && !isLoading && (<>
+				<AddNewTodoForm onAdd={handleAddTodo} />
+
+				{todos.length > 0 ? (
+					<>
+						<h2 className="h5">💪🏻 Stuff I got to do</h2>
+						<TodoList
+							onDelete={handleDelete}
+							onToggle={handleToggle}
+							todos={uncompletedTodos}
+						/>
+
+						<h2 className="h5">🥺 Stuff I've done</h2>
+						<TodoList
+							onDelete={handleDelete}
+							onToggle={handleToggle}
+							todos={completedTodos}
+						/>
+
+						<TodoCounter
+							total={todos.length}
+							uncompleted={uncompletedTodos.length}
+						/>
+					</>
+				) : (
+					<div className="alert alert-warning">
+						You ain't got no todos to do? 🤔
+					</div>
+				)}
+			</>)}
 		</div>
 	);
 }
