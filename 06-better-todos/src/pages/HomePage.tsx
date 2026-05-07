767616a07330e2e2a842ed82f6cac3473ce57dd1
Johan Nordström <johan@digitalvillage.se>
2026-05-07 11:49:58 +0200

    Replaced ordinary, boring links with React Router Links 🤓.

--- 06-better-todos/src/pages/HomePage.tsx
+++ 06-better-todos/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router";
+
 const HomePage = () => {
 	return (
 		<>
 			<title>Better Todos</title>
 			<h1>Welcome to Better Todos!</h1>
 
-			<p>Because when your life is on fire 🔥, you need a todo list.</p>
+			<p>Because when your life is on fire 🔥, you need a <Link to="/todos">todo list</Link>.</p>
 		</>
 	)
 }
--- 06-better-todos/src/pages/partials/Navigation.tsx
+++ 06-better-todos/src/pages/partials/Navigation.tsx
@@ -1,18 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router";
 
 const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="sm">
 			<Container>
-				<Navbar.Brand href="/">📝 Better Todos</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">📝 Better Todos</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/todos">Todos</Nav.Link>
+						<Nav.Link as={NavLink} to="/">Home</Nav.Link>
+						<Nav.Link as={NavLink} to="/todos">Todos</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
