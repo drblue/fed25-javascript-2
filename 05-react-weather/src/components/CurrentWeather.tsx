8b38941e357d99a8f0e93b202b6c0f25bc5a0033
Johan Nordström <johan@digitalvillage.se>
2026-05-07 10:23:01 +0200

    🌤️ React Weather exercise: Show different banner and alt-text depending on daytime or nighttime 🦜🐦‍⬛.

--- 05-react-weather/src/components/CurrentWeather.tsx
+++ 05-react-weather/src/components/CurrentWeather.tsx
@@ -1,4 +1,5 @@
-import imgBanner from "../assets/images/banner.png";
+import imgDayBanner from "../assets/images/day.svg";
+import imgNightBanner from "../assets/images/night.svg";
 import type { CurrentWeatherData } from "../services/OWMAPI.types";
 import WeatherCondition from "./WeatherCondition";
 
@@ -8,11 +9,14 @@ interface CurrentWeatherProps {
 
 const CurrentWeather: React.FC<CurrentWeatherProps> = ({ data }) => {
 	const freshness = new Date(data.dt * 1000);
+	const isDaytime = data.dt > data.sys.sunrise && data.dt < data.sys.sunset;
+	const imgBanner = isDaytime ? imgDayBanner : imgNightBanner;
+	const imgAltText = isDaytime ? "Clouds on a bright sky" : "Clouds on a dark sky with a mooncrest";
 
 	return (
 		<div id="current-weather">
 			<div className="card">
-				<img src={imgBanner} className="card-img-top" alt="Daytime, nighttime, daytime, nighttime" />
+				<img src={imgBanner} className="card-img-top" alt={imgAltText} />
 
 				<div className="card-body">
 					<h5 className="card-title" id="location">
