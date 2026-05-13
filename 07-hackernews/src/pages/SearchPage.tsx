42143cc6b131826ea2e540855e41c0d7746cb58d
Johan Nordström <johan@digitalvillage.se>
2026-05-13 10:59:02 +0200

    Hacker News: Added API service and API type definitions.

--- 07-hackernews/src/pages/SearchPage.tsx
+++ 07-hackernews/src/pages/SearchPage.tsx
@@ -1,31 +1,60 @@
 /* eslint-disable no-constant-binary-expression */
+import { useState } from "react";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const SearchPage = () => {
+	const [error, setError] = useState<string | false>(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [inputSearch, setInputSearch] = useState("");
+	const [searchResult, setSearchResult] = useState(null);  // fix me
+
+	const handleSubmit = (e: React.SubmitEvent) => {
+		e.preventDefault();
+
+		// Prevent smol searches
+		if (inputSearch.trim().length < 2) {
+			alert("Too short search query! Enter at least 3 characters to search for");
+			return;
+		}
+
+		// Search Haxx0r News 🕵🏻‍♂️📰
+		console.log(`Would search for "${inputSearch}" in HN API`);
+	}
+
 	return (
 		<>
 			<title>Search Hacker News</title>
 			<h1>🔎🔦👀</h1>
 
-			<Form className="mb-4">
+			<Form className="mb-4" onSubmit={handleSubmit}>
 				<Form.Group className="mb-3" controlId="searchQuery">
 					<Form.Label>Search Query</Form.Label>
-					<Form.Control type="text" placeholder="Enter your search query" />
+					<Form.Control
+						onChange={(e) => setInputSearch(e.target.value)}
+						placeholder="Enter your search query"
+						type="text"
+						value={inputSearch}
+						required
+					/>
 				</Form.Group>
 
 				<div className="d-flex justify-content-end">
-					<Button variant="success" type="submit">
+					<Button
+						disabled={inputSearch.trim().length < 2}
+						type="submit"
+						variant="success"
+					>
 						Search
 					</Button>
 				</div>
 			</Form>
 
-			{false && <Alert variant='warning'>DIS MY ERROR</Alert>}
+			{error && <Alert variant="warning">{error}</Alert>}
 
-			{false && <p>🤔 Loading...</p>}
+			{isLoading && <p>🤔 Loading...</p>}
 
 			{true && (
 				<div id="search-result">
