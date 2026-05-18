40b852a7c1c6ed21b72defc5d8551ca333023b42
Johan Nordström <johan@digitalvillage.se>
2026-05-18 10:37:19 +0200

    Consume ThemeContext from Navigation and show a theme switcher-button-thingy.

--- 07-hackernews/src/pages/partials/Navigation.tsx
+++ 07-hackernews/src/pages/partials/Navigation.tsx
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink } from "react-router";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 const Navigation = () => {
+	const themeContext = useContext(ThemeContext);
+	if (!themeContext) {
+		throw new Error("Trying to use ThemeContext outside of ThemeContextProvider");
+	}
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="sm">
 			<Container>
@@ -15,6 +23,10 @@ const Navigation = () => {
 						<Nav.Link as={NavLink} end to="/search">Search</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
+
+				<Button onClick={themeContext.toggleTheme} variant="outline-secondary">
+					{themeContext.isDarkMode ? "☀️" : "🌙"}
+				</Button>
 			</Container>
 		</Navbar>
 	)
