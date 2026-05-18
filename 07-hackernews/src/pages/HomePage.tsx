80c485f665fe5625123d436e2c6da1f8c73fe064
Johan Nordström <johan@digitalvillage.se>
2026-05-18 10:32:22 +0200

    Consume ThemeContext from HomePage.

--- 07-hackernews/src/pages/HomePage.tsx
+++ 07-hackernews/src/pages/HomePage.tsx
@@ -1,10 +1,26 @@
+import { useContext } from "react";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const HomePage = () => {
+	const themeContext = useContext(ThemeContext);
+	if (!themeContext) {
+		throw new Error("Trying to use ThemeContext outside of ThemeContextProvider");
+	}
+
 	return (
 		<>
 			<title>Welcome to Hacker News 🕵🏻‍♂️🤓👀!</title>
 			<h1>Welcome to Hacker News 🕵🏻‍♂️🤓👀!</h1>
+
+			<div className="mb-4">
+				<p>Theme: {themeContext.isDarkMode ? "🌖 Dark" : "☀️ Light"}</p>
+				<Button
+					onClick={themeContext.toggleTheme}
+					variant="secondary"
+				>Toggle theme</Button>
+			</div>
 
 			<Link className="btn btn-primary" to="/search">
 				Use the Search, you must!
