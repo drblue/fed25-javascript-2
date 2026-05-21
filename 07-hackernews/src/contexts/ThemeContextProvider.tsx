601e62a0defc7d0d01723c4a49074525b7031379
Johan Nordström <johan@digitalvillage.se>
2026-05-21 10:55:24 +0200

    Get theme from `localStorage` (without using a custom hook 😕).

--- 07-hackernews/src/contexts/ThemeContextProvider.tsx
+++ 07-hackernews/src/contexts/ThemeContextProvider.tsx
@@ -6,12 +6,28 @@ interface ThemeContextProviderProps {
 }
 
 const ThemeContextProvider = ({ children }: ThemeContextProviderProps) => {
-	const [isDarkMode, setIsDarkMode] = useState(true);
+	const [isDarkMode, setIsDarkMode] = useState(() => {
+		// This will only be executed on the FIRST render when no state exists
+		// The return value will be used as the initial value for the state
+
+		console.log("Getting initalState for `isDarkMode` from localStorage `hn_darkmode`...");
+		const localStorage_hn_darkmode = window.localStorage.getItem("hn_darkmode") ?? "true";
+
+		return localStorage_hn_darkmode === "true";
+	});
 	const isLightMode = !isDarkMode;
 
 	const toggleTheme = () => {
+		// Set new state
 		setIsDarkMode(!isDarkMode);
+
+		// Save new value to localStorage
+		// N.B.! `isDarkMode` has not been changed yet as React batches updating of states!
+		console.log("💾 Saving theme to localStorage...");
+		window.localStorage.setItem("hn_darkmode", String(!isDarkMode));
 	}
+
+	console.log("ThemeContextProvider rendering, isDarkMode is:", isDarkMode);
 
 	return (
 		<ThemeContext.Provider value={{ isDarkMode, isLightMode, toggleTheme }}>
