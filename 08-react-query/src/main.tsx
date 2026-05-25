ab41b45a546db1564e0a4c2fd3a5c73761e76579
Johan Nordström <johan@digitalvillage.se>
2026-05-25 09:49:35 +0200

    Installed TanStack Query. Created and provided a `QueryClient` to the app.

--- 08-react-query/src/main.tsx
+++ 08-react-query/src/main.tsx
@@ -1,12 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
+
+// Create a query client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</StrictMode>
 );
