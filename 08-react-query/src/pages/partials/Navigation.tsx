1e1ee5bcf9db2c3a25c2c5744c370fe920948b38
Johan Nordström <johan@digitalvillage.se>
2026-05-25 11:42:53 +0200

    Added number of products to Navbar to demonstrate query deduplication (as long as the two queries have the same `queryKey`).

--- 08-react-query/src/pages/partials/Navigation.tsx
+++ 08-react-query/src/pages/partials/Navigation.tsx
@@ -1,9 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import Badge from "react-bootstrap/Badge";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink } from "react-router";
+import { getProducts } from "../../services/BortakvallAPI";
 
 const Navigation = () => {
+	const { data: products, isSuccess } = useQuery({
+		queryKey: ["products"],
+		queryFn: getProducts,
+	});
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="sm">
 			<Container>
@@ -12,7 +20,13 @@ const Navigation = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/products">🍬 Products</Nav.Link>
+						<Nav.Link as={NavLink} end to="/products">
+							🍬 Products
+							{" "}
+							<Badge bg="success" pill>
+								{isSuccess ? products.length : "-"}
+							</Badge>
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
