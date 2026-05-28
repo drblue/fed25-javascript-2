833339353929d1fe6702ff7ab5d403082aa0e7f3
Johan Nordström <johan@digitalvillage.se>
2026-05-28 11:48:59 +0200

    Installed TanStack Query, configured `QueryClient` and added React Query Devtools.

--- 09-react-query-todos/src/main.tsx
+++ 09-react-query-todos/src/main.tsx
@@ -2,11 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 5,  // 5 minutes 🥴
+			gcTime: 1000 * 60 * 60,  // 1 hour 🚮
+		},
+	},
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</StrictMode>
 );
