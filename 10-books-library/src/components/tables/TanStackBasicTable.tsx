1c770398ff368a3e4eb3392c43c7ce17a9ddd45f
Johan Nordström <johan@digitalvillage.se>
2026-06-09 10:43:02 +0200

    Moved column definitions to BooksPage and refactored TanStackBookTable into the generic TanStackBasicTable component.

---
+++ 10-books-library/src/components/tables/TanStackBasicTable.tsx
@@ -0,0 +1,48 @@
+import { flexRender, getCoreRowModel, useReactTable, type ColumnDef } from "@tanstack/react-table";
+import Table from "react-bootstrap/Table";
+
+interface TanStackBasicTableProps<T> {
+	columns: ColumnDef<T>[];
+	data: T[];
+}
+
+const TanStackBasicTable = <T,>({ columns, data }: TanStackBasicTableProps<T>) => {
+	const table = useReactTable({
+		columns,
+		data,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<Table bordered responsive striped>
+			<thead>
+				{table.getHeaderGroups().map(headerGroup => (
+					<tr key={headerGroup.id}>
+						{headerGroup.headers.map(header => (
+							<th key={header.id} colSpan={header.colSpan}>
+								{header.isPlaceholder
+									? null
+									: flexRender(header.column.columnDef.header, header.getContext())
+								}
+							</th>
+						))}
+					</tr>
+				))}
+			</thead>
+
+			<tbody>
+				{table.getRowModel().rows.map(row => (
+					<tr key={row.id}>
+						{row.getVisibleCells().map(cell => (
+							<td key={cell.id}>
+								{flexRender(cell.column.columnDef.cell, cell.getContext())}
+							</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</Table>
+	)
+}
+
+export default TanStackBasicTable;
--- 10-books-library/src/pages/BooksPage.tsx
+++ 10-books-library/src/pages/BooksPage.tsx
@@ -1,6 +1,53 @@
+import type { ColumnDef } from "@tanstack/react-table";
+import { Link } from "react-router";
 import WarningAlert from "../components/alerts/WarningAlert";
-import TanStackBookTable from "../components/tables/TanStackBookTable";
+import TanStackBasicTable from "../components/tables/TanStackBasicTable";
 import useBooks from "../hooks/useBooks";
+import type { Book } from "../services/BooksAPI.types";
+
+const columns: ColumnDef<Book>[] = [
+	{
+		header: "Book Info",
+		columns: [
+			{
+				header: "Title",
+				accessorKey: "title",  // {book.title}
+			},
+			{
+				header: "Pages",
+				accessorKey: "pages",  // {book.pages}
+				cell: ({ getValue }) => (
+					<span className="d-block text-end">{getValue()}</span>
+				),
+			},
+			{
+				header: "Published",
+				accessorKey: "published",
+				cell: ({ getValue }) => (
+					<span className="d-block text-end">{getValue()}</span>
+				),
+			},
+		],
+	},
+	{
+		header: "Author Info",
+		columns: [
+			{
+				header: "Name",
+				accessorKey: "author.name",
+				cell: ({ getValue, row }) => (
+					<Link to={"/authors/" + row.original.author.id}>
+						{getValue()}
+					</Link>
+				),
+			},
+			{
+				header: "Birthdate",
+				accessorKey: "author.date_of_birth",
+			},
+		],
+	},
+];
 
 const BooksPage = () => {
 	const { data: books, isError, isLoading } = useBooks();
@@ -18,7 +65,7 @@ const BooksPage = () => {
 
 			{isLoading && <p>Loading books...</p>}
 
-			{books && <TanStackBookTable books={books} />}
+			{books && <TanStackBasicTable columns={columns} data={books} />}
 		</>
 	);
 };
