3490c1d6353fbf136ea2e46e8195980f0689085d
Johan Nordström <johan@digitalvillage.se>
2026-06-11 12:03:06 +0200

    Installed Motion and made homepage (a lot) more ugly.

--- 10-books-library/src/pages/HomePage.tsx
+++ 10-books-library/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import Image from "react-bootstrap/Image";
 import imgIpadFrozen from "../assets/images/ipad-frozen.jpg";
 
@@ -5,14 +6,38 @@ const HomePage = () => {
 	return (
 		<>
 			<title>Books Library</title>
-			<h1>Books Library</h1>
+			<motion.h1
+				initial={{ y: -100, scale: 0 }}
+				animate={{
+					y: 0,
+					scale: 1,
+					rotateY: 360,
+					transition: {
+						duration: 1,
+					},
+				}}
+			>Books Library</motion.h1>
 
-			<Image
-				src={imgIpadFrozen}
-				alt="Cartoon drawing with a child holding up a book to a parent and saying 'This iPad is frozen'"
-				title="This iPad is frozen"
-				fluid
-			/>
+			<motion.div
+				initial={{ opacity: 0, scale: 0 }}
+				animate={{
+					scale: 1,
+					opacity: 1,
+					transition: {
+						duration: 0.5,
+						type: "spring",
+						mass: 0.4,
+						damping: 8,
+					},
+				}}
+			>
+				<Image
+					src={imgIpadFrozen}
+					alt="Cartoon drawing with a child holding up a book to a parent and saying 'This iPad is frozen'"
+					title="This iPad is frozen"
+					fluid
+				/>
+			</motion.div>
 		</>
 	)
 }
