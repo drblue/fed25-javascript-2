50291d38c7c5884545911228dab79abddc986fd4
Johan Nordström <johan@digitalvillage.se>
2026-06-15 11:45:37 +0200

    Get a single document from Firestore 🔥🔥.

--- 11-firebase-todos/src/pages/TodoPage.tsx
+++ 11-firebase-todos/src/pages/TodoPage.tsx
@@ -1,23 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Alert from "react-bootstrap/Alert";
 import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import { Link, useParams } from "react-router";
 import ConfirmationModal from "../components/ConfirmationModal";
 import type { Todo } from "../types/Todo.types";
-
-const todo: Todo = {
-	_id: "Xi6VigUyelerlFbSHKTZ",
-	title: "Learn to fake better data 😅",
-	completed: true,
-};
+import { doc, getDoc } from "firebase/firestore";
+import { todosCol } from "../libs/firebase";
 
 const TodoPage = () => {
+	const [error, setError] = useState<Error | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [todo, setTodo] = useState<Todo | null>(null);
 	const [showDeleteModal, setShowDeleteModal] = useState(false);
 	const { id } = useParams();
 
 	const handleDelete = () => {
 		setShowDeleteModal(false);
 		console.log("Would delete todo with id:", id)
+	}
+
+	useEffect(() => {
+		const getTodo = async () => {
+			setError(null);
+			setIsLoading(true);
+			setTodo(null);
+
+			// Get reference to document in "todos" collection
+			// and a snapshot of the document
+			const docRef = doc(todosCol, id);
+			const snapshot = await getDoc(docRef);
+
+			// Check if document actually existed
+			if (!snapshot.exists()) {
+				setError(new Error("Document not found"));
+				setIsLoading(false);
+				return;
+			}
+
+			// Transform document to match `Todo` type
+			const data = {
+				...snapshot.data() as Todo,
+				_id: snapshot.id,
+			}
+
+			// Update state
+			setTodo(data);
+			setIsLoading(false);
+		}
+		getTodo();
+	}, [id]);
+
+	if (error) {
+		return <Alert variant="danger">
+			<Alert.Heading>Doh! Bad stuff happened. Try again later?</Alert.Heading>
+			<p><strong>Error:</strong> {error.message}</p>
+		</Alert>
+	}
+
+	if (isLoading) {
+		return <p>Loading todo... nom nom nom 🍪</p>
 	}
 
 	return todo && (
