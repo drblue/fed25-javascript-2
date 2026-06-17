35d8de421267e28da4989fa6a5b3596ccdf30450
Johan Nordström <johan@digitalvillage.se>
2026-06-17 11:52:00 +0200

    Installed React Hook Form and react-hook-form-ified our `TodoForm`.

--- 11-firebase-todos/src/components/TodoForm.tsx
+++ 11-firebase-todos/src/components/TodoForm.tsx
@@ -1,6 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import type { NewTodo } from "../types/Todo.types";
 
 interface TodoFormProps {
@@ -10,41 +10,48 @@ interface TodoFormProps {
 }
 
 const TodoForm: React.FC<TodoFormProps> = ({ className, isCreating = false, onAdd }) => {
-	const [inputTodoTitle, setInputTodoTitle] = useState("");
+	const { handleSubmit, register, reset, formState: { errors } } = useForm<NewTodo>({
+		defaultValues: {
+			completed: false,
+		},
+	});
 
-	const handleSubmit = async (e: React.SubmitEvent) => {
-		e.preventDefault();
+	const onFormSubmit: SubmitHandler<NewTodo> = async (formData) => {
+		console.log("Yum, I got some validated data 😋:", formData);
 
 		// Tell parent to create a new todo with `inputTodoTitle` as the title
-		await onAdd({
-			title: inputTodoTitle.trim(),
-			completed: false,
-		});
+		await onAdd(formData);
 
 		// Clear input field
-		setInputTodoTitle("");
+		reset();
 	}
 
 	return (
-		<Form onSubmit={handleSubmit} className={className}>
+		<Form onSubmit={handleSubmit(onFormSubmit)} className={className}>
 			<Form.Group controlId="title" className="mb-3">
 				<Form.Label>Title</Form.Label>
 				<Form.Control
-					isInvalid={inputTodoTitle.trim().length > 0 && inputTodoTitle.trim().length < 3}
-					onChange={(e) => setInputTodoTitle(e.target.value)}
+					isInvalid={!!errors.title}
 					placeholder="Learn about GTD"
 					type="text"
-					value={inputTodoTitle}
-					autoFocus
-					required
+					{...register("title", {
+						minLength: {
+							message: "Title has to be at least 3 characters",
+							value: 3,
+						},
+						required: {
+							message: "Title is required",
+							value: true,
+						},
+					})}
 				/>
 				<Form.Control.Feedback type="invalid">
-					Please enter 3 characters or more.
+					{errors.title?.message || "Invalid input"}
 				</Form.Control.Feedback>
 			</Form.Group>
 
 			<Button
-				disabled={inputTodoTitle.trim().length < 3 || isCreating}
+				disabled={isCreating}
 				type="submit"
 				variant="success"
 			>Save</Button>
