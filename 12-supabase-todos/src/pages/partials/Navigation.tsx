d743357683f3d3f2bdc8002ddc3cbcb6b6b6e9e0
Johan Nordström <johan@digitalvillage.se>
2026-08-13 11:20:04 +0200

    Show different menu items depending on whether a user is logged in or not.

--- 12-supabase-todos/src/pages/partials/Navigation.tsx
+++ 12-supabase-todos/src/pages/partials/Navigation.tsx
@@ -2,8 +2,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 const Navigation = () => {
+	const { currentUser } = useAuth();
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="sm">
 			<Container>
@@ -12,7 +15,17 @@ const Navigation = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} to="/todos" end>Todos</Nav.Link>
+						{currentUser ? (
+							<>
+								<Nav.Link as={NavLink} to="/todos" end>Todos</Nav.Link>
+								<Nav.Link as={NavLink} to="/logout">Logout</Nav.Link>
+							</>
+						) : (
+							<>
+								<Nav.Link as={NavLink} to="/login">Log in</Nav.Link>
+								<Nav.Link as={NavLink} to="/signup">Sign up</Nav.Link>
+							</>
+						)}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
