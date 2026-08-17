8a7cd2a9f32cddb4dac4202feb3476dbc1bff338
Johan Nordström <johan@digitalvillage.se>
2026-08-17 10:04:53 +0200

    🤳🏻 Show user photo or name in Navbar. Fall back to user email if neither photo nor name is set.

--- 12-supabase-todos/src/pages/partials/Navigation.tsx
+++ 12-supabase-todos/src/pages/partials/Navigation.tsx
@@ -1,12 +1,15 @@
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link, NavLink } from "react-router";
 import useAuth from "../../hooks/useAuth";
+import { getProfileMetadata } from "../../lib/profile";
 
 const Navigation = () => {
 	const { currentUser } = useAuth();
+	const { display_name, photo_url } = getProfileMetadata(currentUser);
 
 	return (
 		<Navbar bg="dark" variant="dark" expand="sm">
@@ -15,13 +18,22 @@ const Navigation = () => {
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
+					<Nav className="ms-auto align-items-center">
 						{currentUser ? (
 							<>
 								<Nav.Link as={NavLink} to="/todos" end>Todos</Nav.Link>
 
 								<NavDropdown
-									title={currentUser.email}
+									title={photo_url ? (
+										<Image
+											alt="Photo of user (probably)"
+											className="object-fit-cover"
+											height={32}
+											src={photo_url}
+											width={32}
+											roundedCircle
+										/>
+									) : display_name || currentUser.email}
 								>
 									<NavDropdown.Item as={NavLink} to="/profile">Update profile</NavDropdown.Item>
 									<NavDropdown.Divider />
