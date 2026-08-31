284426fdd269a0bc0d07766ac72eaff81977d0ed
Johan Nordström <johan@digitalvillage.se>
2026-08-31 14:22:27 +0200

    Loop dem todos 🎢.

--- 17-vue-basics/src/App.vue
+++ 17-vue-basics/src/App.vue
@@ -3,12 +3,17 @@ import ClickCounter from "@/components/ClickCounter.vue";
 import ParrotParrot from "./components/ParrotParrot.vue";
 import SalaryNegotiator from "./components/SalaryNegotiator.vue";
 import MouseTracker from "./components/MouseTracker.vue";
+import TodoList from "./components/TodoList.vue";
 </script>
 
 <template>
 	<div class="container">
 		<h1>Vue 3 Basics</h1>
 		<h2>How you like them Vue's?</h2>
+
+		<hr class="my-4" />
+
+		<TodoList />
 
 		<hr class="my-4" />
 
---
+++ 17-vue-basics/src/components/TodoList.vue
@@ -0,0 +1,43 @@
+<!--
+Bind something 📿:
+
+Longhand:
+v-bind:key="todo.id"
+
+Shorthand:
+:key="todo.id"
+-->
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Todo } from '@/types';
+
+const todos = ref<Todo[]>([
+	{ id: 1, title: 'Learn Vue.js', completed: true },
+	{ id: 2, title: 'Learn not to procrastinate', completed: false },
+	{ id: 3, title: 'Take over the world', completed: false },
+])
+</script>
+
+<template>
+	<div>
+		<h2>Todo List</h2>
+
+		<ul class="list-group">
+			<li
+				class="list-group-item"
+				:key="todo.id"
+				v-for="todo in todos"
+			>
+				<span :class="{ done: todo.completed }">{{ todo.title }}</span>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<style lang="css" scoped>
+.done {
+	color: #bbb;
+	font-style: italic;
+	text-decoration: line-through;
+}
+</style>
