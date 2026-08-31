5715a166fdef2a75c15a837bd25686f82581699c
Johan Nordström <johan@digitalvillage.se>
2026-08-31 14:00:11 +0200

    🪤🐁🛤️. Mousetracker.

--- 17-vue-basics/src/App.vue
+++ 17-vue-basics/src/App.vue
@@ -2,12 +2,17 @@
 import ClickCounter from "@/components/ClickCounter.vue";
 import ParrotParrot from "./components/ParrotParrot.vue";
 import SalaryNegotiator from "./components/SalaryNegotiator.vue";
+import MouseTracker from "./components/MouseTracker.vue";
 </script>
 
 <template>
 	<div class="container">
 		<h1>Vue 3 Basics</h1>
 		<h2>How you like them Vue's?</h2>
+
+		<hr class="my-4" />
+
+		<MouseTracker />
 
 		<hr class="my-4" />
 
---
+++ 17-vue-basics/src/components/MouseTracker.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const mouseX = ref(0)
+const mouseY = ref(0)
+
+const updateMousePosition = (e: MouseEvent) => {
+	mouseX.value = e.offsetX
+	mouseY.value = e.offsetY
+}
+</script>
+
+<template>
+	<div class="mouse-area" @mousemove="updateMousePosition">
+		<span>{{ mouseX }}, {{ mouseY }}</span>
+	</div>
+</template>
+
+<style lang="css" scoped>
+.mouse-area {
+	align-items: center;
+	background-color: #ccc;
+	border: 4px solid #888;
+	cursor: pointer;
+	display: flex;
+	height: 200px;
+	justify-content: center;
+	width: 200px;
+}
+</style>
