938174cb9687ec9da7f4586d89c60444511b369f
Johan Nordström <johan@digitalvillage.se>
2026-08-31 13:43:15 +0200

    🦜 Demo of `v-model` for 2-way databinding. Also `@submit.prevent` to prevent default event behaviour.

--- 17-vue-basics/src/components/ParrotParrot.vue
+++ 17-vue-basics/src/components/ParrotParrot.vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const parrotInput = ref("")
+
+const submit = () => {
+	alert("🦜: " + parrotInput.value)
+}
+
+const reset = () => {
+	parrotInput.value = ""
+}
 </script>
 
 <template>
 	<div>
-		<form class="mb-3">
+		<form class="mb-3" @submit.prevent="submit" @reset="reset">
 			<label for="parrotInput">Say something</label>
 			<input
 				class="form-control"
 				id="parrotInput"
 				type="text"
+				v-model="parrotInput"
 			/>
 			<button class="btn btn-success" type="submit">Submit</button>
 			<button class="btn btn-danger" type="reset">Reset</button>
 		</form>
 
 		<div class="alert alert-success">
-			<span role="img" aria-label="parrot">🦜</span> ...
+			<span role="img" aria-label="parrot">🦜</span> {{ parrotInput }}
 		</div>
 	</div>
 </template>
